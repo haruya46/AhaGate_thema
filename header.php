9df4882308b231a6ca3c60933a75a9e0008da041
平山晴也 <puzu70@gmail.com>
2025-05-11 15:58:03 +0900

    メタディスクリプションの追加

--- header.php
+++ header.php
@@ -3,6 +3,12 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="AhaGate（アハゲート）は、初心者から中級者までを対象としたオンラインプログラミングスクールです。
+  月4回のZoomセッションやLINEでの質問対応を通じて、受講生の自走力と継続力を育成します。
+  ビギナーコースではHTML、CSS、JavaScript、PHPなどの基礎を学び、プロコースではLaravelを用いたポートフォリオ制作を行います。
+  月額30,000円で入会金や違約金は不要、途中解約も可能です。
+  学習習慣がなくても、講師がスケジュール管理をサポートします。詳細は公式サイトをご覧ください。">
+
   <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
 
